06f328a2af8346c7750075a345518824c3b5a1df
Michal Szymczyk <michal.szymczyk@withsecure.com>
2025-03-15 20:50:22 +0100

    GPU required

--- v3-mha.py
+++ v3-mha.py
@@ -2,14 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-batch_size = 32
-block_size = 8
+batch_size = 64
+block_size = 256
 max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 torch.manual_seed(1337)
 
@@ -72,6 +75,8 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False) # (n_embd, head_size) (32, 32)
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size))) # block_size = context_length = 8
 
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         """"
         wei = torch.einsum("bthd,bthd->bht", q, k) / (C ** 0.5) # (B, H, T, T)
@@ -84,16 +89,65 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * C**(-0.5) # (32, 8, 32) @ (32, 32, 8) -> (32, 8, 8)
         wei - wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
         wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
         
         v = self.value(x) # (B, T, C) (32, 8, 32)
         out = wei @ v # (32, 8, 8) @ (32, 8, 32) -> (32, 8, 32) [*]
-        return out
+        return out # (32, 8, 32)
+    
+class MultiHeadAttention(nn.Module):
+    """ Multi-head attention mechanism
+    """
+
+    def __init__(self, num_heads, head_size):
+        super(MultiHeadAttention, self).__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)]) 
+        self.proj = nn.Linear(head_size * num_heads, n_embd) # (n_embd, n_embd) (32, 32)
+        self.dropout = nn.Dropout(dropout)
+    
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1) # concatenate over C dimension, n_embd
+        out = self.dropout(self.proj(out))
+        return  out 
+        # [32, 8, 32]
+
+class FeedForwad(nn.Module):
+    def __init__(self, n_embd):
+        super(FeedForwad, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd ),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """Tansformer block"""
+
+    def __init__(self, n_embd, n_head):
+        super(Block, self).__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForwad(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super(BigramLanguageModel, self).__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # (vocab_size, n_embd) (65, 32)
         self.position_embedding_table = nn.Embedding(block_size, n_embd) # (block_size, n_embd) (8, 32)
-        self.sa_head = Head(n_embd) 
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size) # (n_embd, vocab_size) (32, 65)
 
     def forward(self, idx, targets=None): # n_embd =-> C
@@ -102,7 +156,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B, T, C), (B, block_size, n_embd), (32, 8, 32)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, n_embd) (block_size, n_embd), (8, 32)
         x = tok_emb + pos_emb # (B, T, n_embd) (B, block_size, n_embd), (32, 8, 32)
-        x = self.sa_head(x) # (B, T, n_embd) (B, block_size, n_embd), (32, 8, 32) - one head of self-attention [*]
+        x = self.blocks(x) # (B, T, n_embd) (B, block_size, n_embd), (32, 8, 32) - one head of self-attention [*]
+        x = self.ln_f(x) # (B, T, n_embd) (B, block_size, n_embd), (32, 8, 32) - feed forward
         logits = self.lm_head(tok_emb) # (B, T, vocab_size) (B, block_size, vocab_size), (32, 8, 65)
 
 
@@ -142,7 +197,7 @@ for iter in range(max_iters):
 
     logits, loss = model(x, y)
 
-    optimizer.zero_grad()
+    optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
 
