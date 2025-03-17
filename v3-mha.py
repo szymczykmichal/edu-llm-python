05ccec9db104851d2466da9dbe2e2c0e43947c1f
Michal Szymczyk <michal.szymczyk@withsecure.com>
2025-03-17 22:09:41 +0100

    parameter changes

--- v3-mha.py
+++ v3-mha.py
@@ -2,17 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-batch_size = 64
-block_size = 256
+batch_size = 16 # how many independent sequences will we process in parallel?
+block_size = 32 # what is the maximum context length for predictions?
 max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4
+eval_interval = 100
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
-dropout = 0.2
+n_embd = 64
+n_head = 4
+n_layer = 4
+dropout = 0.0
 
 torch.manual_seed(1337)
 
